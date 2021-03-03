d1b6297b23ed2c0100d7f6d25a8bde0214149a11
guschenxi <chenxijack@gmail.com>
2021-03-03 12:21:09 +0100

    none

--- src/dmAppointment.ts
+++ src/dmAppointment.ts
@@ -9,10 +9,31 @@ function listen(): Action<SDSContext, SDSEvent> {
     return send('LISTEN')
 }
 
-const grammar: { [index: string]: { person?: string, day?: string, time?: string } } = {
+const grammar: { [index: string]: { person?: string, day?: string, whole_day?:string, time?: string } } = {
     "John": { person: "John Appleseed" },
+    "Monica": { person: "Monica Svensson" },
+    "on Monday": { day: "Monday" },
+    "on Tuesday": { day: "Tuesday" },
+    "on Wednesday": { day: "Wednesday" },
+    "on Thursday": { day: "Thursday" },
     "on Friday": { day: "Friday" },
+    "on Saturday": { day: "Saturday" },
+    "on Sunday": { day: "Sunday" },
+    "yes": { whole_day: "the whole day" },
+    "no": { whole_day: "not the whole day" },
+    "of course": { whole_day: "the whole day" },
+    "no way": { whole_day: "not the whole day" },
+    "at eight": { time: "8:00" },
+    "at nine": { time: "9:00" },
+    "at eleven": { time: "11:00" },
     "at ten": { time: "10:00" },
+    "at twelve": { time: "12:00" },
+    "at thirteen": { time: "13:00" },
+    "at fourteen": { time: "14:00" },
+    "at fifteen": { time: "15:00" },
+    "at sixteen": { time: "16:00" },
+    "at seventeen": { time: "17:00" },
+    "at eighteen": { time: "18:00" },
 }
 
 
@@ -58,15 +79,87 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = ({
         },
         day: {
             initial: "prompt",
-            on: { ENDSPEECH: "init" },
+            on: {
+                RECOGNISED: [{
+                    cond: (context) => "day" in (grammar[context.recResult] || {}),
+                    actions: assign((context) => { return { day: grammar[context.recResult].day } }),
+                    target: "whole_day"
+
+                },
+                { target: ".nomatch" }]
+            },
             states: {
                 prompt: {
                     entry: send((context) => ({
                         type: "SPEAK",
                         value: `OK. ${context.person}. On which day is your meeting?`
-                    }))
+                    })),
+                    on: { ENDSPEECH: "ask" }
                 },
+                ask: {
+                    entry: listen()
+                },
+                nomatch: {
+                    entry: say("Sorry I don't understand which day you mean."),
+                    on: { ENDSPEECH: "prompt" }
+                }
             }
-        }
+        },
+        whole_day: {
+            initial: "prompt",
+            on: {
+                RECOGNISED: [{
+                    cond: (context) => "whole_day" in (grammar[context.recResult] || {}),
+                    actions: assign((context) => { return { whole_day: grammar[context.recResult].whole_day } }),
+                    target: "time"
+
+                },
+                { target: ".nomatch" }]
+            },
+            states: {
+                prompt: {
+                    entry: send((context) => ({
+                        type: "SPEAK",
+                        value: `OK. ${context.person}, ${context.day}. Will it take the whole day?`
+                    })),
+                    on: { ENDSPEECH: "ask" }
+                },
+                ask: {
+                    entry: listen()
+                },
+                nomatch: {
+                    entry: say("Sorry I don't understand. Say yes or no."),
+                    on: { ENDSPEECH: "prompt" }
+                }
+            }
+        },
+        time: {
+            initial: "prompt",
+            on: {
+                RECOGNISED: [{
+                    cond: (context) => "time" in (grammar[context.recResult] || {}),
+                    actions: assign((context) => { return { time: grammar[context.recResult].time } }),
+                    target: "confirm"
+
+                },
+                { target: ".nomatch" }]
+            },
+            states: {
+                prompt: {
+                    entry: send((context) => ({
+                        type: "SPEAK",
+                        value: `OK. ${context.person}, ${context.day}, ${context.whole_day}. What time is your meeting?`
+                    })),
+                    on: { ENDSPEECH: "ask" }
+                },
+                ask: {
+                    entry: listen()
+                },
+                nomatch: {
+                    entry: say("Sorry I don't understand. Say a time."),
+                    on: { ENDSPEECH: "prompt" }
+                }
+            }
+        },
     }
 })
